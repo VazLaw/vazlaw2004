c75882097890f3ca24ce1bf6f772337e6c6ca676
VazLaw <140375881+VazLaw@users.noreply.github.com>
2023-07-26 14:06:00 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+    strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film maker","Vaz Law"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true,
+});
+$('.counter').countUp();
---
+++ js/tab2.js
@@ -0,0 +1,17 @@
+// JavaScript Documentfunction openCity2(evt, cityName) {
+function openCity2(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen2").click();
